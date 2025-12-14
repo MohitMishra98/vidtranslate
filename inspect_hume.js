fda2e60d181c80a882aa02725580545f73370c4b
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2025-12-14 04:11:45 +0000

    creating the project

--- inspect_hume.js
+++
@@ -1,9 +0,0 @@
-import { HumeClient } from "hume";
-import dotenv from "dotenv";
-dotenv.config();
-
-const client = new HumeClient({ apiKey: process.env.HUME_API_KEY });
-console.log(
-  "TTS methods:",
-  Object.getOwnPropertyNames(Object.getPrototypeOf(client.tts))
-);
